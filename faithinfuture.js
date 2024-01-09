c9405fff075766bc91c638d9dbe860c2d3fef1e8
Ravikiran <ravikiranpatil088@gmail.com>
2024-01-09 15:57:12 +0530

    second commit

--- faithinfuture.js
+++ faithinfuture.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     var header = document.getElementById("main-header");
+    var dashboard = document.getElementById("achievements");
 
     window.addEventListener("scroll", function () {
         if (window.scrollY >= 425) {
-            header.style.display = "none";
+            header.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+         
         } else {
             header.style.backgroundColor = "transparent";
             header.style.display = "flex";
